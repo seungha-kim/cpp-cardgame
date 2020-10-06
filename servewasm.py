4e83cb62f46d7ca76e782613a72ddb999fe3b0d5
Dan Kim <dan@protopie.io>
2020-10-06 21:31:52 +0900

    servewasm.py: disable cache

--- servewasm.py
+++ servewasm.py
@@ -6,13 +6,23 @@ import socketserver
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
-Handler.extensions_map.update({
+# https://stackoverflow.com/a/28708920
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_my_headers()
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_my_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+
+MyHTTPRequestHandler.extensions_map.update({
     '.wasm': 'application/wasm',
 })
 
 socketserver.TCPServer.allow_reuse_address = True
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("", PORT), MyHTTPRequestHandler) as httpd:
     httpd.allow_reuse_address = True
     print("serving at port", PORT)
     httpd.serve_forever()
